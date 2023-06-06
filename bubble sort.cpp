8158536381d77ef8f3eaa1754db82c192d222202
TrungNT <trunghy2k4111@gmail.com>
2023-06-06 16:07:14 +0700

    sắp xếp nổi bọt

---
+++ bubble sort.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+
+void bubblesort(int a[], int n){
+	for(int i=0;i<n-1;i++){
+		for(int j=0;j<n-i-1;j++){
+			if(a[j]>a[j+1]){
+			int tmp=a[j];
+			a[j]=a[j+1];
+			a[j+1]=tmp;
+			}
+		}
+		for(int i=0;i<n;i++){
+			printf("%d ",a[i]);
+		}
+		printf("\n");
+	}
+}
+int main(){
+	int n;
+	scanf("%d",&n);
+	int a[n];
+	for(int i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	bubblesort(a,n);
+}
