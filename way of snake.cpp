4316e8089c7ab332366ce4b275ed6aae7519de93
TrungNT <trunghy2k4111@gmail.com>
2023-06-07 10:23:16 +0700

    đường đi của con rắn

---
+++ way of snake.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	int a[n][n];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(int i=0;i<n;i++){
+		if(i%2==0){
+			for(int j=0;j<n;j++){
+				printf("%d ",a[i][j]);
+			}
+		}
+		else{
+			for(int j=n-1;j>=0;j--){
+				printf("%d ",a[i][j]);
+			}
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
